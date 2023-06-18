303cd5455dc542b63601095c630c52e0bf2e80db
Lucas Peixoto <lspeixotodev@gmail.com>
2023-06-18 12:39:50 -0300

    Remove unused imports

--- src/main/java/com/lspeixotodev/blogrestapi/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/lspeixotodev/blogrestapi/security/JwtAuthenticationEntryPoint.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
